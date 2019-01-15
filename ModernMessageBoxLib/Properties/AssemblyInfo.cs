b3dbb23155fe41b4a945f714c5f770d6317d8428
Saber <hv0905@outlook.com>
2019-01-15 18:03:24 +0800

    Update AssemblyInfo.cs

--- ModernMessageBoxLib/Properties/AssemblyInfo.cs
+++ ModernMessageBoxLib/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: NeutralResourcesLanguage("")]
 
